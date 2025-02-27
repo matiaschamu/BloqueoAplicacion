cddb36e1d1731be3bed8b2d0a3af324e0bfd9f99
matiaschamu <matiaschamu@gmail.com>
2025-02-27 17:58:29 -0300

    Version 4.0.0.13, Se agrega una referencia en la aplicacion del Test a CosturaFody para incluir todas las DLL en un ejecutable, Se agrega un icono.

--- TestBloqueoAplicacion/Properties/AssemblyInfo.cs
+++ TestBloqueoAplicacion/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("TestBloqueoSowart")]
+[assembly: AssemblyTitle("BloqueoSowart")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestBloqueoSowart")]
+[assembly: AssemblyProduct("BloqueoSowart")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.13")]
+[assembly: AssemblyFileVersion("4.0.0.13")]
